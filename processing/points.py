f574583c774c2477a8afa71a6125a593cfa62f5b
Max Malynowsky <github@maxmalynowsky.com>
2022-01-02 22:31:40 -0500

    remove overlapping points inside deep v-shapes

--- processing/points.py
+++ processing/points.py
@@ -15,19 +15,20 @@ query_1 = """
 query_2 = """
     DROP TABLE IF EXISTS {table_out};
     CREATE TABLE {table_out} AS
-    SELECT DISTINCT ON (geom)
+    SELECT
         a.fid,
         (ST_Dump(
-            ST_SnapToGrid(
+            ST_Union(ST_SnapToGrid(
                 ST_Difference(ST_Points(
                     ST_Segmentize(a.geom, {segment})
                 ), b.geom)
-            , {snap})
+            , {snap}))
         )).geom::GEOMETRY(Point, 4326) AS geom
     FROM {table_in1} AS a
     CROSS JOIN {table_in2} AS b
+    GROUP BY a.fid
     UNION ALL
-    SELECT DISTINCT ON (geom)
+    SELECT
         a.fid,
         (ST_Dump(
             ST_Boundary(ST_Difference(a.geom, b.geom))
@@ -36,8 +37,32 @@ query_2 = """
     CROSS JOIN {table_in2} AS b;
     CREATE INDEX ON {table_out} USING GIST(geom);
 """
+query_3 = """
+    DROP TABLE IF EXISTS {table_out};
+    CREATE TABLE {table_out} AS
+    SELECT
+        geom,
+        count(*)
+    FROM {table_in}
+    GROUP BY geom;
+    CREATE INDEX ON {table_out} USING GIST(geom);
+"""
+query_4 = """
+    DROP TABLE IF EXISTS {table_out};
+    CREATE TABLE {table_out} AS
+    SELECT DISTINCT ON (a.geom)
+        b.fid,
+        a.geom
+    FROM {table_in1} AS a
+    LEFT JOIN {table_in2} AS b
+    ON a.geom = b.geom
+    WHERE a.count = 1;
+    CREATE INDEX ON {table_out} USING GIST(geom);
+"""
 drop_tmp = """
     DROP TABLE IF EXISTS {table_tmp1};
+    DROP TABLE IF EXISTS {table_tmp2};
+    DROP TABLE IF EXISTS {table_tmp3};
 """
 
 
@@ -52,9 +77,20 @@ def main(cur, name, *_):
         table_in2=Identifier(f'{name}_03_tmp1'),
         segment=Literal(config['segment']),
         snap=Literal(config['snap']),
+        table_out=Identifier(f'{name}_03_tmp2'),
+    ))
+    cur.execute(SQL(query_3).format(
+        table_in=Identifier(f'{name}_03_tmp2'),
+        table_out=Identifier(f'{name}_03_tmp3'),
+    ))
+    cur.execute(SQL(query_4).format(
+        table_in1=Identifier(f'{name}_03_tmp3'),
+        table_in2=Identifier(f'{name}_03_tmp2'),
         table_out=Identifier(f'{name}_03'),
     ))
     cur.execute(SQL(drop_tmp).format(
         table_tmp1=Identifier(f'{name}_03_tmp1'),
+        table_tmp2=Identifier(f'{name}_03_tmp2'),
+        table_tmp3=Identifier(f'{name}_03_tmp3'),
     ))
     logger.info(name)
