6e615ae718988744f8f3d77316d7b2f6d6a94361
Max Malynowsky <github@maxmalynowsky.com>
2025-02-13 09:15:46 +0100

    fix order of thresholds

--- app/attempt.py
+++ app/attempt.py
@@ -9,11 +9,11 @@ logger = logging.getLogger(__name__)
 
 def main(conn, name, file, layer, segment, snap, *_):
     if config["retry"].lower() in ("yes", "on", "true", "1"):
-        for x in range(9):
-            for y in range(9):
-                for z in [1, 10]:
-                    curr_segment = Decimal(config["segment"]) * (x + 1)
-                    curr_snap = Decimal(config["snap"]) * (y + 1) * z
+        for z in range(9):
+            for y in [1, 10]:
+                for x in range(9):
+                    curr_segment = Decimal(config["segment"]) * (z + 1)
+                    curr_snap = Decimal(config["snap"]) * (x + 1) * y
                     try:
                         logger.info(f"{name} segment={curr_segment} snap={curr_snap}")
                         points.main(conn, name, file, layer, curr_segment, curr_snap)
